2d4863b87d80203e04cbf40a61db6914835713fb
David <haydal.tmps@gmail.com>
2019-07-12 18:11:05 +0000

    button.js and cards.js added

---
+++ assets/js/buttons.js
@@ -0,0 +1,7 @@
+// This is the onclick function, when action button is click on
+
+
+function buttonQuestion() { 
+	document.getElementById("game-title").style.display="none"; 
+	document.getElementById("question").style.display="block"; 
+}
---
+++ assets/js/cards.js
@@ -0,0 +1,14 @@
+// This is the onclick function, when images are click on
+
+function myFunction1() {
+  document.getElementById("card1").style.border = "15px solid green";
+}
+
+function myFunction2() {
+  document.getElementById("card2").style.border = "15px solid green";
+}
+
+function myFunction3() {
+  document.getElementById("card3").style.border = "15px solid green";
+}
+
