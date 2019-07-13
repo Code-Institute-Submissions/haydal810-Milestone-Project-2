396a13d5b2ad98df1cf9ec1c1c7a24261b642086
David <haydal.tmps@gmail.com>
2019-07-13 09:47:47 +0000

    restart button assigned a reload function onclick

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -1,7 +1,11 @@
-// This is the onclick function, when action button is click on
+// This is the onclick function, when action button is click on, it makes welcome text div disappear and and other div appear instead
 
 
 function buttonQuestion() { 
 	document.getElementById("game-title").style.display="none"; 
 	document.getElementById("question").style.display="block"; 
 }
+
+function buttonRestart() {
+    window.location.reload();
+} 
